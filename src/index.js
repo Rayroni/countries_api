69d0b5ad143497336ee4b08ff2abf6e8d1ffd1fa
Rayroni <seiya.void@gmail.com>
2022-07-26 10:19:27 -0300

    adiação graphql

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// initialize a GraphQL client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://countries.trevorblades.com'
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// write a GraphQL query that asks for names and codes for all countries
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
+
+// create a component that renders a select input for coutries
+function CountrySelect() {
+  const [country, setCountry] = useState('US');
+  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+
+  if (loading || error) {
+    return <p>{error ? error.message : 'Loading...'}</p>;
+  }
+
+  return (
+    <select value={country} onChange={event => setCountry(event.target.value)}>
+      {data.countries.map(country => (
+        <option key={country.code} value={country.code}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+ReactDOM.render(<CountrySelect />, document.getElementById('root'));
